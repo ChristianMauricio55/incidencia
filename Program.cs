39f5263d8af9411c12bc9ded406e466639c6e6bf
ChristianMauricio55 <63982529+ChristianMauricio55@users.noreply.github.com>
2026-03-02 12:32:32 -0600

    Configure application to run on specified port
    Set the application to run on a specified port from environment variable or default to 8080.

--- Program.cs
+++ Program.cs
@@ -53,5 +53,7 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Run($"http://0.0.0.0:{port}");
 
-app.Run();
+//app.Run();
